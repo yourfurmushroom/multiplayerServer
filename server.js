5b5aa0ede43c9d1e1cd5a4cbd426cd88db8e1b55
YiFeng Li <s7887177jp@gmail.com>
2022-05-15 23:19:25 +0800

    a

--- server.js
+++ server.js
@@ -1,48 +1,74 @@
 var uuid = require('uuid-random');
 const WebSocket = require('ws')
-
 const wss = new WebSocket.WebSocketServer({port:8080}, ()=> {
 	console.log('server started')
 })
 
-var playerList=[];
-var bulletList=[];
- 
-var playersData = {
-	"type" : "playerData"
+var clientList={};
+function SendMessage(client, obj){
+	const json = JSON.stringify(obj);
+	console.log(`SendMessage: ${json}`);
+	client.send(json);
+}
+function InitClient(client){
+	id = uuid();
+	client.id = id;
+	client.player = {
+			"position":{
+				"x":0,
+				"y":0,
+				"z":0
+			},
+			"rotation":{
+				"x": 0,
+				"y": 0,
+				"z": 0,
+				"w": 0
+			}
+		};
+	clientList[id] = client;
 }
 
+function OnUpdatePlayerPosition(clientId, args){
+	var client = clientList[clientId];
+	client.player.position = args.position;
+}
+function OnUpdatePlayerRotation(clientId, args){
+	var client = clientList[clientId];
+	client.player.rotation = args.rotation;
+}
 wss.on('connection', function connection(client){
 
-	client.id = uuid();	
+	InitClient(client);
+	playerConnectedArgs = {
+		"messageType": "PlayerConnected",
+		"id": "" + client.id
+	};
+	SendMessage(client, playerConnectedArgs);
+	client.on('message', (json) => {
+		console.log(`client ${client.id} recive message: ${json}`);
 
-	console.log(`Client ${client.id} Connected!`)
-	
-	var currentClient = playersData[""+client.id]
-	playerList.push(currentClient);
+		var args = JSON.parse(json);
+		switch (json.messageType) {
+			case "UpdatePlayerPosition":
+				OnUpdatePlayerPosition(client.id, args)
+				break;
+			case "UpdatePlayerRotation":
+				OnUpdatePlayerRotation(client.id, args)
+				break;
+			default:
+				break;
+		}
 
-	client.send(`{"id": "${client.id}"}`)
-
-	client.on('message', (data) => {
-		var dataJSON = JSON.parse(data)
-		console.log(playerList.length)
-		console.log(playersData)
-		playerList.forEach(player => {
-			console.log("Player Message")
-			//console.log(dataJSON)
-			console.log(client.id)
-		});
-		
 	})
 
 	client.on('close', () => {
 		console.log('This Connection Closed!')
 		console.log("Removing Client: " + client.id)
-		
 	})
 
+	console.log(`Client ${client.id} Connected!`)
 })
-
 wss.on('listening', () => {
 	console.log('listening on 8080')
 })
